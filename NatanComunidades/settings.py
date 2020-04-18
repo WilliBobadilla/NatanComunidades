6368f7f1c71f453eb925ebcd9a65484288323897
WilliBobadilla <Willi1997.1@gmail.com>
2020-04-18 15:10:53 -0400

    changing somes configurations and adding NatanApp

--- NatanComunidades/settings.py
+++ NatanComunidades/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k72yfjb)ysh1j^+8!3rn)j^8lu5oae5@k5kev2#y*7s@i3in$g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'NatanApp'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
