ef2b65ee1224d3c33c982102443fb142781e3504
WilliBobadilla <Willi1997.1@gmail.com>
2020-04-21 13:48:27 -0400

    changing some things in the format of the popups in maps

--- NatanComunidades/NatanApp/views.py
+++ NatanComunidades/NatanApp/views.py
@@ -112,10 +112,13 @@ def cargar_lista_articulos(request):
 
 
 def mapa(request):
+  if request.user.is_staff:
+    print("soy staff y que ")
   if not request.user.is_authenticated:
-        return render(request,'prueba_login.html')
+      return render(request,'prueba_login.html')
   lista=consulta_datos()
-  data = {"geo": lista} # al final enviamos esto 
+  cant_comunidades=len(Comunidad.objects.all())
+  data = {"geo": lista,"cantidad_comunidades":cant_comunidades} # al final enviamos esto 
   return render (request,'map.html',data)
 
 
@@ -148,11 +151,13 @@ def mapa_cargar(request):
     datos_comunidades=Comunidad(nombre=nombre,responsable=responsable,latitud=latitud,longitud=longitud,meta=meta,disp=disp,entregado=entregado,listo=listo)
     datos_comunidades.save()
     lista=consulta_datos()
-    data = {"geo": lista} # al final enviamos esto 
+    cant_comunidades=len(Comunidad.objects.all())
+    data = {"geo": lista,"cantidad_comunidades":cant_comunidades} # al final enviamos esto 
     return render(request,'mapa_agregar_data.html',data)
   else:
     lista=consulta_datos()
-    data = {"geo": lista} # al final enviamos esto 
+    cant_comunidades=len(Comunidad.objects.all())
+    data = {"geo": lista,"cantidad_comunidades":cant_comunidades} # al final enviamos esto 
     return render(request,'mapa_agregar_data.html',data)
 
 
