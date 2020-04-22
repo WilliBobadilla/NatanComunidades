6ac3b3cffaa4df6a62b14f4d2eec787feb2f9e4c
WilliBobadilla <Willi1997.1@gmail.com>
2020-04-22 14:44:53 -0400

    trying to test the image storage

--- NatanComunidades/urls.py
+++ NatanComunidades/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from NatanComunidades.NatanApp.views import *
+from django.conf import settings # new
+from django.urls import path, include # new
+from django.conf.urls.static import static # new
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +33,5 @@ urlpatterns = [
     path('cargar_donacion', cargar_donacion),
     path('mapa_cargar',mapa_cargar)
 ]
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
