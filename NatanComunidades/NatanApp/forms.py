27be9993dec300ec7d362a542312a1630b7665ed
nandyme <fernandomellone2@gmail.com>
2020-04-22 23:12:04 -0400

    ya se muestran los resultados en la lista

--- NatanComunidades/NatanApp/forms.py
+++ NatanComunidades/NatanApp/forms.py
@@ -35,7 +35,6 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-from django import forms
 from .models import Donacion
 
 
